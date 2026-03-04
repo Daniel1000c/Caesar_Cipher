31e6492516509abc33e53a32e05ead2cfee19097
Daniel Velez <velezdaniel516@gmail.com>
2026-03-04 11:22:39 -0500

    Define function name and write out function steps

---
+++ cipher.rb
@@ -0,0 +1,23 @@
+#Caesar Cipher Function
+def caesar_cipher(string,shift)
+  #Create result variable to store new string
+  result = ""
+
+  #Loop through each character in the string
+
+  #Get current character from the string
+  #Check if character is lowercase letter or uppercase letter
+    #If it is:
+    #Convert letter to character code
+    #Shift letter by shift amount
+    #Wrap character around modulo
+    #Convert said character back to letter
+    #Concat to result string
+  
+  #If character is not a letter, add to result unchanged
+  
+  #Return final result string after loop has finished
+  result
+end
+
+caesar_cipher('Hello',5)
